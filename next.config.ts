8ae67f6dca7c3fdf4c0f622354786dd4f481663a
Dhrubo Sarker Avroo <dhrubosarkeravroo@gmail.com>
2026-07-13 20:16:23 +0600

    fix: add typescript dependency

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 const nextConfig = {
-  output: 'export', 
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-} as any;
+  
+};
 
 export default nextConfig;
